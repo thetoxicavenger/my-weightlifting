f910a43a8a4efa73f721af5e3e474d80895f81b2
Mitch Cravens <mcravens12@gmail.com>
2019-07-20 12:40:58 -0700

    added order to seed files so that foreign key contraints synced up and didnt throw error.

--- server/db/seeds/1563651379_workouts.js
+++ server/db/seeds/1563651379_workouts.js
@@ -15,11 +15,11 @@ exports.seed = function(knex, Promise) {
           },
           {
             name: 'Day 4',
-            img: 'https://static-s.aa-cdn.net/img/ios/1121042545/bcdf9e6b1a0223b37e57de428c63dfb1?v=1',
+            img: 'https://cdn.iconscout.com/icon/free/png-256/workout-40-1100757.png',
           },
           {
             name: 'Day 5',
-            img: 'https://apprecs.org/ios/images/app-icons/256/c7/985518279.jpg',
+            img: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/circle-exercise-fitness-orange-run-running-workout-icon-45827.png',
           },
           
         ]);
